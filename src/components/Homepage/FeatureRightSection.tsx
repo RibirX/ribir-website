184b3f409b44b83f56d24f448ad157cdc90d794d
thinkingjimmy <thinkingjimmy@outlook.com>
2023-12-27 15:08:08 +0800

    Update feature sections styling

--- src/components/Homepage/FeatureRightSection.tsx
+++ src/components/Homepage/FeatureRightSection.tsx
@@ -7,9 +7,9 @@ export default function FeatureRightSection({title, description, image}) {
         <div className="bg-white md:w-6/12 max-[640px]:1/5 md:order-last sm:order-last max-[640px]:order-first">
           <img src={image} alt="Image description" />
         </div>
-        <div className="self-center text-pretty max-w-xl md:ml-4 sm:ml-4 max-[640px]:ml-0">
-          <div className='text-5xl font-bold'>{title}</div>
-          <div className="text-xl mt-6">
+        <div className="self-center text-pretty max-w-xl md:ml-4 sm:ml-4 max-[640px]:ml-0 max-[640px]:mr-4">
+          <div className='text-5xl font-bold max-[640px]:text-3xl'>{title}</div>
+          <div className="text-xl mt-6 max-[640px]:text-base">
           {description}
           </div>
         </div>
